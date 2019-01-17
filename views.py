2423b6737490e3323a9ae1385d9941718c7d8bd3
oxpeter <oxpeter+git@gmail.com>
2019-01-17 08:45:30 -0500

    corrected placeholder for get_queryset

--- views.py
+++ views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
 
+from django.views import generic
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
 class IndexView(LoginRequiredMixin, generic.ListView):
     login_url='/login/'
     
-    template_name = 'dc_management/index.html'
-    context_object_name = 'project_list'
+    template_name = 'datacatalog/index.html'
+    context_object_name = 'data_list'
 
     def get_queryset(self):
         # get the model that will be primarily indexed"""
         # return model
-    
+        return None
+        
     def get_context_data(self, **kwargs):
-        # get useful model objects here
+        "get useful model objects here"
         context = super(IndexView, self).get_context_data(**kwargs)
         context.update({
                         # 'object_list'   : object_list,  
